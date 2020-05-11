657f4e7222c0037922368c605ebbae1441a0bcd4
Connor Brookes <connor.brookes@student.manchester.ac.uk>
2020-05-11 14:45:49 +0100

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Magic 8 Ball'),
+            backgroundColor: Colors.blue[700],
+          ),
+          body: MagicEight(),
+        ),
       ),
     );
+
+class MagicEight extends StatefulWidget {
+  @override
+  _MagicEightState createState() => _MagicEightState();
+}
+
+class _MagicEightState extends State<MagicEight> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+}
